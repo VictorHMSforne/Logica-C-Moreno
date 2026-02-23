08a0eea8d79734c55d26c3e893ef4c3aefa2b0dd
VictorHMSforne <vmendessforne@gmail.com>
2026-02-23 14:07:57 -0300

    Feito o passo 2 do exercício

--- Atividade1.c
+++ Atividade1.c
@@ -1,11 +1,14 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
+#include <locale.h>
 
-/*passo1:
-Criar struct contendo nome, idade e telefone e fazer a inserção do dado, apresentando os dados no final (para conferencia)
+/*
+passo1:
+Criar struct contendo nome, idade e telefone e fazer a inserção do dado, apresentando os dados no final (para conferencia)  - X
 
 passo2:
-Converter strutura para um vetor de 5 elementos. Fazer a entrada dos dados e apresentar no final
+Converter strutura para um vetor de 5 elementos. Fazer a entrada dos dados e apresentar no final - X
 
 passo3:
 Construir um procedimento para entrada dos dados e  outro procedimento para saída
@@ -28,26 +31,38 @@ typedef struct {
 	char telefone[16];
 } pessoa;
 
-pessoa p; // é Realizado a Instância da struct
+pessoa p[4]; // é Realizado a Instância da struct
 
 
 int main(){
-	printf("Digite o nome da pessoa:\n");
-	fgets(p.nome,sizeof(p.nome),stdin);
+	setlocale(LC_ALL,"Portuguese");
 	
-	printf("Digite a idade dessa pessoa:\n");
-	scanf("%d",&p.idade);
-	fflush(stdin);
+	int i;
+	for(i=0;i<=4;i++){
+		printf("Digite o nome %dª da pessoa:\n", i+1);
+		fgets(p[i].nome,sizeof(p[i].nome),stdin);
+		p[i].nome[strcspn(p[i].nome,"\n")] = '\0';
+		
+		printf("Digite a idade dessa pessoa:\n");
+		scanf("%d",&p[i].idade);
+		fflush(stdin);
+		
+		printf("Digite o telefone dessa pessoa:\n");
+		fgets(p[i].telefone,sizeof(p[i].telefone),stdin);
+		printf("------------------------------------\n");
+		printf("\n");
+	}
 	
-	printf("Digite o telefone dessa pessoa:\n");
-	fgets(p.telefone,sizeof(p.telefone),stdin);
+	
 	
 	printf("--------------------------\n");
 	printf("     EXIBINDO EM TELA:    \n");
 	printf("--------------------------\n");
+	for(i=0;i<=4;i++){
+		printf("Nome:     %s\n",p[i].nome);
+		printf("Idade:    %d\n",p[i].idade);
+		printf("Telefone: %s",p[i].telefone);
+	}
 	
-	printf("Nome:      %s",p.nome);
-	printf("Idade:     %d\n",p.idade);
-	printf("Telefone:  %s",p.telefone);
 	
 }
